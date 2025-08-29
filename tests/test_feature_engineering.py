adf353baebb3b00cfddb86838fa4c87f6b840ced
konradhorber <konrad.horber@gmail.com>
2025-08-29 14:25:28 +0200

    Remove data loading from feature engineering tests to isolate feature engineering logic

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -5,9 +5,4 @@ from src.feature_engineering import FeatureEngineer
 
 
 def test_feature_engineering():
-    data_directory = Path("src/data/")
-    loader = TrainingDataLoader(data_directory)
-    df = loader.load_data()
-
     feature_engineer = FeatureEngineer()
-    X, Y = feature_engineer.engineer_full(df)
--- tests/test_training_data_loader.py
+++ tests/test_training_data_loader.py
@@ -5,4 +5,3 @@ from src.training_data_loader import TrainingDataLoader
 def test_load_data():
     data_directory = Path("src/data/")
     loader = TrainingDataLoader(data_directory)
-    loader.load_data()
